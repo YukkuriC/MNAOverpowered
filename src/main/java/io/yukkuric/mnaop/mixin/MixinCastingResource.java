67f5354e3382c3512f55bfa41baefdd84b9e7c0e
YukkuriC <799433638@qq.com>
2025-10-28 23:20:23 +0800

    bugfix: change wrap position

--- src/main/java/io/yukkuric/mnaop/mixin/MixinCastingResource.java
+++ src/main/java/io/yukkuric/mnaop/mixin/MixinCastingResource.java
@@ -1,16 +1,14 @@
 package io.yukkuric.mnaop.mixin;
 
 import com.mna.api.capabilities.resource.ICastingResource;
+import com.mna.api.capabilities.resource.SimpleCastingResource;
 import io.yukkuric.mnaop.MNAOPConfig;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ICastingResource.class)
-public class MixinCastingResource {
-    @Inject(method = "hungerAffectsRegenRate", at = @At("HEAD"), remap = false, cancellable = true)
-    void IgnoreSaturation(CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(MNAOPConfig.FastRegenManaIgnoresSaturation());
+@Mixin(SimpleCastingResource.class)
+public abstract class MixinCastingResource implements ICastingResource {
+    @Override
+    public boolean hungerAffectsRegenRate() {
+        return MNAOPConfig.FastRegenManaIgnoresSaturation();
     }
 }
