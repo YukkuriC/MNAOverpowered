f25dfde11e85f626f04622596284af737fbda03c
YukkuriC <799433638@qq.com>
2025-10-29 20:51:26 +0800

    another copy of jinja codegen I really should learn about publishing to PYPI

---
+++ codegen/templates/forge.java
@@ -0,0 +1,39 @@
+package io.yukkuric.mnaop;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+
+@Mod.EventBusSubscriber(modid = MNAOPMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class MNAOPConfig {
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+    // entries
+    {%- for grp,lines in group_val(data,'type') %}
+    public ForgeConfigSpec.{{grp.capitalize()}}Value{% for line in lines %}
+            Cfg_{{line.name}} = BUILDER.comment("{{line.descrip}}").define("{{line.name}}", {{line.default}})
+            {%- if loop.last %};{% else %},{% endif %}
+        {%- endfor %}
+    {%- endfor %}
+    static final ForgeConfigSpec SPEC = BUILDER.build();
+
+    // interfaces
+    {%- for line in data %}
+    public static {{line.type}} {{line.name}}() {
+        return cache_FastRegenManaIgnoresSaturation;
+    }
+    {%- endfor %}
+
+    // caches
+    {%- for grp,lines in group_val(data,'type') %}
+    private static {{grp}} {% for line in lines %}cache_{{line.name}}{%- if loop.last %};{% else %}, {% endif %}{%- endfor %}
+    {%- endfor %}
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event) {
+        // cache values according to template
+        {%- for line in data %}
+        cache_{{line.name}} = Cfg_{{line.name}}.get();
+        {%- endfor %}
+    }
+}
--- src/main/java/io/yukkuric/mnaop/MNAOPConfig.java
+++ src/main/java/io/yukkuric/mnaop/MNAOPConfig.java
@@ -5,14 +5,13 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 
-// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
-// Demonstrates how to use Forge's config APIs
 @Mod.EventBusSubscriber(modid = MNAOPMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MNAOPConfig {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
     // entries
-    private static final ForgeConfigSpec.BooleanValue Cfg_FastRegenManaIgnoresSaturation = BUILDER.comment("Whether to log the dirt block on common setup").define("FastRegenManaIgnoresSaturation", true);
+    public ForgeConfigSpec.BooleanValue
+            Cfg_FastRegenManaIgnoresSaturation = BUILDER.comment("Speed-up casting resource regeneration, regardless of player's food saturation").define("FastRegenManaIgnoresSaturation", true);
     static final ForgeConfigSpec SPEC = BUILDER.build();
 
     // interfaces
