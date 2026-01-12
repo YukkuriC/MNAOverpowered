6b8dd090a1811687f42d45096dbc822619391ced
YukkuriC <799433638@qq.com>
2026-01-13 01:03:41 +0800

    use general collector & uploader

--- collector/collector.py
+++ collector/collector.py
@@ -1,18 +1,4 @@
-import os, glob, shutil
+from YukkuriC.minecraft.collector import *
+import os
 
-os.chdir(os.path.dirname(__file__))
-
-
-def do_collect():
-    res = []
-    for path in glob.glob('../build/libs/*.jar'):
-        if any(x in path for x in ('source', 'dev', 'all')):
-            continue
-        print(path)
-        shutil.copy(path, '.')
-        res.append(path)
-    return res
-
-
-if __name__ == '__main__':
-    do_collect()
+do_collect_simple(os.path.dirname(__file__))
