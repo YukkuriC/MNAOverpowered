ff76efbf9330047ec476b4eb0e7ee71093353377
YukkuriC <799433638@qq.com>
2026-01-28 21:43:14 +0800

    remove unused hooks

--- src/main/java/io/yukkuric/mnaop/MNAOPMod.java
+++ src/main/java/io/yukkuric/mnaop/MNAOPMod.java
@@ -2,7 +2,6 @@ package io.yukkuric.mnaop;
 
 import com.mojang.logging.LogUtils;
 import io.yukkuric.mnaop.command.MNAOPCommands;
-import io.yukkuric.mnaop.construct.TaskExRegistry;
 import io.yukkuric.mnaop.magichem.MagiChemEvents;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -26,8 +25,6 @@ public class MNAOPMod {
     public MNAOPMod() {
         // IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         IEventBus eventBus = MinecraftForge.EVENT_BUS;
-        eventBus.register(this);
-        eventBus.register(TaskExRegistry.class);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, MNAOPConfig.SPEC);
         eventBus.addListener((RegisterCommandsEvent event) -> MNAOPCommands.register(event.getDispatcher()));
     }
