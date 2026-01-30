b11f9ea424f50c14a30fd29be4bfb13563c966d0
YukkuriC <799433638@qq.com>
2026-01-30 20:05:32 +0800

    damn

--- collector/uploader.py
+++ collector/uploader.py
@@ -2,5 +2,5 @@ from YukkuriC.minecraft.uploader import *
 
 load_cfg_changelog()
 
-push_file = build_pusher('mod_name,game_version,mod_version')
+push_file = build_pusher('mod_name,game_version,mod_version', platform='forge')
 push_all('.', push_file)
