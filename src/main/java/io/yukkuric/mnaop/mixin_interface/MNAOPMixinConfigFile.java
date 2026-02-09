157b72569fd345c3922c09e8703c37fa2eea89a8
YukkuriC <799433638@qq.com>
2026-02-09 22:11:51 +0800

    refactor: separated config file

---
+++ src/main/java/io/yukkuric/mnaop/mixin_interface/MNAOPMixinConfigFile.java
@@ -0,0 +1,83 @@
+package io.yukkuric.mnaop.mixin_interface;
+
+import io.yukkuric.mnaop.MNAOPMod;
+import net.minecraft.util.StringUtil;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
+
+public class MNAOPMixinConfigFile {
+    static final String CONFIG_PATH = "config/mnaoverpowered-mixin.ini";
+    static final String KEY_DENIED_MIXINS = "DeniedMixinClasses";
+    static final String KEY_DENIED_CONSTRUCT_TASKS = "DeniedConstructTasks";
+
+    final Path configPath = Path.of(CONFIG_PATH);
+    final Set<String> deniedMixinClasses = new HashSet<>();
+    final Set<String> deniedConstructTasks = new HashSet<>();
+    void dumpSet(String raw, Set<String> target) {
+        target.clear();
+        for (var sub : raw.split(",")) {
+            sub = sub.trim();
+            if (StringUtil.isNullOrEmpty(sub)) continue;
+            target.add(sub);
+        }
+    }
+    void processConfigLine(String key, String val) {
+        switch (key) {
+            case KEY_DENIED_MIXINS -> dumpSet(val, deniedMixinClasses);
+            case KEY_DENIED_CONSTRUCT_TASKS -> dumpSet(val, deniedConstructTasks);
+        }
+    }
+
+    void load() {
+        tryRun(this::loadInner, "read");
+    }
+    void save() {
+        tryRun(this::saveInner, "writ");
+    }
+
+    interface RunnableWithError {
+        void run() throws Throwable;
+    }
+    private void tryRun(RunnableWithError task, String name) {
+        try {
+            task.run();
+        } catch (Throwable e) {
+            MNAOPMod.LOGGER.error("Error when {}ing mixin config: {}", name, e.getLocalizedMessage());
+            try (var sw = new StringWriter()) {
+                try (var writer = new PrintWriter(sw)) {
+                    e.printStackTrace(writer);
+                    MNAOPMod.LOGGER.error(sw.toString());
+                }
+            } catch (Throwable ignored) {
+            }
+        }
+    }
+    private void loadInner() throws IOException {
+        if (!Files.exists(configPath)) return;
+        var lines = Files.readString(configPath, StandardCharsets.UTF_8).split("\n");
+        for (var line : lines) {
+            if (line.startsWith("#")) continue;
+            var raw = line.split("=");
+            if (raw.length != 2) {
+                MNAOPMod.LOGGER.error("Error when reading mixin config line: need exactly 1 '='\nline: " + line);
+                continue;
+            }
+            processConfigLine(raw[0].trim(), raw[1].trim());
+        }
+    }
+    private void saveInner() throws IOException {
+        var sb = new StringBuilder();
+        sb.append("# mixin classes that will be ignored before load; input their names separated by comma\n");
+        sb.append("# e.g. MixinCastingResource, MixinInstantConstructShlorper\n");
+        sb.append(String.format("%s=%s\n", KEY_DENIED_MIXINS, String.join(", ", deniedMixinClasses)));
+        sb.append("# construct tasks that won't be registered; input their registry IDs (without namespace) separated by comma\n");
+        sb.append("# e.g. batch_provide_materia\n");
+        sb.append(String.format("%s=%s\n", KEY_DENIED_CONSTRUCT_TASKS, String.join(", ", deniedConstructTasks)));
+        Files.writeString(configPath, sb.toString(), StandardCharsets.UTF_8);
+    }
+}
--- src/main/java/io/yukkuric/mnaop/mixin_interface/MNAOPMixinPlugin.java
+++ src/main/java/io/yukkuric/mnaop/mixin_interface/MNAOPMixinPlugin.java
@@ -2,96 +2,30 @@ package io.yukkuric.mnaop.mixin_interface;
 
 import com.llamalad7.mixinextras.MixinExtrasBootstrap;
 import io.yukkuric.mnaop.MNAOPMod;
-import net.minecraft.util.StringUtil;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.*;
-import java.util.*;
+import java.util.List;
+import java.util.Set;
 
 public class MNAOPMixinPlugin implements IMixinConfigPlugin {
-    static final String CONFIG_PATH = "config/mnaoverpowered-mixin.ini";
-    static final String KEY_DENIED_MIXINS = "DeniedMixinClasses";
-    static final String KEY_DENIED_CONSTRUCT_TASKS = "DeniedConstructTasks";
-
     public static MNAOPMixinPlugin INSTANCE;
     public MNAOPMixinPlugin() {
         INSTANCE = this;
     }
     public static boolean isConstructTaskDenied(String path) {
-        return INSTANCE.deniedConstructTasks.contains(path);
+        return INSTANCE.CFG.deniedConstructTasks.contains(path);
     }
 
-    final Set<String> deniedMixinClasses = new HashSet<>();
-    final Set<String> deniedConstructTasks = new HashSet<>();
-    void dumpSet(String raw, Set<String> target) {
-        target.clear();
-        for (var sub : raw.split(",")) {
-            sub = sub.trim();
-            if (StringUtil.isNullOrEmpty(sub)) continue;
-            target.add(sub);
-        }
-    }
-    void processConfigLine(String key, String val) {
-        switch (key) {
-            case KEY_DENIED_MIXINS -> dumpSet(val, deniedMixinClasses);
-            case KEY_DENIED_CONSTRUCT_TASKS -> dumpSet(val, deniedConstructTasks);
-        }
-    }
+    final MNAOPMixinConfigFile CFG = new MNAOPMixinConfigFile();
     @Override
     public void onLoad(String s) {
         MixinExtrasBootstrap.init(); // do NOT forget this using MixinExtras
 
-        // try load config file
-        var configPath = Path.of(CONFIG_PATH);
-        if (Files.exists(configPath)) {
-            try {
-                var lines = Files.readString(configPath, StandardCharsets.UTF_8).split("\n");
-                for (var line : lines) {
-                    if (line.startsWith("#")) continue;
-                    var raw = line.split("=");
-                    if (raw.length != 2) {
-                        MNAOPMod.LOGGER.error("Error when reading mixin config line: need exactly 1 '='\nline: " + line);
-                        continue;
-                    }
-                    processConfigLine(raw[0].trim(), raw[1].trim());
-                }
-            } catch (Throwable e) {
-                MNAOPMod.LOGGER.error("Error when reading mixin config: {}", e.getLocalizedMessage());
-                try (var sw = new StringWriter()) {
-                    try (var writer = new PrintWriter(sw)) {
-                        e.printStackTrace(writer);
-                        MNAOPMod.LOGGER.error(sw.toString());
-                    }
-                } catch (Throwable ignored) {
-                }
-            }
-        }
-
-        // try create config file
-        try {
-            var sb = new StringBuilder();
-            sb.append("# mixin classes that will be ignored before load; input their names separated by comma\n");
-            sb.append("# e.g. MixinCastingResource, MixinInstantConstructShlorper\n");
-            sb.append(String.format("%s=%s\n", KEY_DENIED_MIXINS, String.join(", ", deniedMixinClasses)));
-            sb.append("# construct tasks that won't be registered; input their registry IDs (without namespace) separated by comma\n");
-            sb.append("# e.g. batch_provide_materia\n");
-            sb.append(String.format("%s=%s\n", KEY_DENIED_CONSTRUCT_TASKS, String.join(", ", deniedConstructTasks)));
-            Files.writeString(configPath, sb.toString(), StandardCharsets.UTF_8);
-        } catch (Throwable e) {
-            MNAOPMod.LOGGER.error("Error when writing mixin config: {}", e.getLocalizedMessage());
-            try (var sw = new StringWriter()) {
-                try (var writer = new PrintWriter(sw)) {
-                    e.printStackTrace(writer);
-                    MNAOPMod.LOGGER.error(sw.toString());
-                }
-            } catch (Throwable ignored) {
-            }
-        }
+        // try load/save config file
+        CFG.load();
+        CFG.save();
     }
     @Override
     public String getRefMapperConfig() {
@@ -101,7 +35,7 @@ public class MNAOPMixinPlugin implements IMixinConfigPlugin {
     public boolean shouldApplyMixin(String targetCls, String mixinCls) {
         var raw = mixinCls.split("\\.");
         var mixinClsName = raw[raw.length - 1];
-        if (deniedMixinClasses.contains(mixinClsName)) {
+        if (CFG.deniedMixinClasses.contains(mixinClsName)) {
             MNAOPMod.LOGGER.warn("Denied mixin class: {}", mixinCls);
             return false;
         }
