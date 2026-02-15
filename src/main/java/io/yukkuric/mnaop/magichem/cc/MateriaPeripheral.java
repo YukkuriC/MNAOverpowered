385ac9269c8579d6db66d17f0c1b25debca3bf30
YukkuriC <799433638@qq.com>
2026-02-15 13:31:13 +0800

    provisioning & sorting

--- src/main/java/io/yukkuric/mnaop/magichem/cc/MateriaPeripheral.java
+++ src/main/java/io/yukkuric/mnaop/magichem/cc/MateriaPeripheral.java
@@ -2,13 +2,15 @@ package io.yukkuric.mnaop.magichem.cc;
 
 import com.aranaira.magichem.block.entity.ext.AbstractMateriaStorageMultiTypeBlockEntity;
 import com.aranaira.magichem.block.entity.ext.AbstractMateriaStorageSingleTypeBlockEntity;
+import com.aranaira.magichem.foundation.IMateriaProvisionRequester;
+import com.aranaira.magichem.foundation.IMateriaSortingRequester;
 import com.aranaira.magichem.item.MateriaItem;
 import dan200.computercraft.api.lua.LuaException;
 import dan200.computercraft.api.lua.LuaFunction;
 import dan200.computercraft.api.peripheral.GenericPeripheral;
 import io.yukkuric.mnaop.MNAOPMod;
 
-import java.util.List;
+import java.util.*;
 
 import static com.aranaira.magichem.block.entity.ext.AbstractMateriaStorageMultiTypeBlockEntity.materiaMap;
 
@@ -27,6 +29,7 @@ public class MateriaPeripheral implements GenericPeripheral {
         return materiaMap.get(raw);
     }
 
+    // multi-type container
     @LuaFunction(mainThread = true)
     public List<String> getMateriaTypes(AbstractMateriaStorageMultiTypeBlockEntity be) {
         return be.getMateriaTypes().stream().map(x -> x.getMateriaName()).toList();
@@ -47,20 +50,38 @@ public class MateriaPeripheral implements GenericPeripheral {
         return be.getCurrentStockPercent(mat);
     }
 
+    // single-type container
     @LuaFunction(mainThread = true)
     public String getMateriaType(AbstractMateriaStorageSingleTypeBlockEntity be) {
         return be.getMateriaType().getMateriaName();
     }
     @LuaFunction(mainThread = true)
-    public int getAmount(AbstractMateriaStorageSingleTypeBlockEntity be) throws LuaException {
+    public int getAmount(AbstractMateriaStorageSingleTypeBlockEntity be) {
         return be.getCurrentStock();
     }
     @LuaFunction(mainThread = true)
-    public int getLimit(AbstractMateriaStorageSingleTypeBlockEntity be, String id) throws LuaException {
+    public int getLimit(AbstractMateriaStorageSingleTypeBlockEntity be, String id) {
         return be.getStorageLimit();
     }
     @LuaFunction(mainThread = true)
-    public float getPercent(AbstractMateriaStorageSingleTypeBlockEntity be, String id) throws LuaException {
+    public float getPercent(AbstractMateriaStorageSingleTypeBlockEntity be, String id) {
         return be.getCurrentStockPercent();
+    }
+
+    // provide & sort
+    @LuaFunction(mainThread = true)
+    public boolean needProvisioning(IMateriaProvisionRequester be) {
+        return be.needsProvisioning();
+    }
+    @LuaFunction(mainThread = true)
+    public Map<String, Integer> getProvisioningNeeds(IMateriaProvisionRequester be) {
+        var raw = be.getProvisioningNeeds();
+        var ret = new HashMap<String, Integer>();
+        for (var kv : raw.entrySet()) ret.put(kv.getKey().getMateriaName(), kv.getValue());
+        return ret;
+    }
+    @LuaFunction(mainThread = true)
+    public boolean needSorting(IMateriaSortingRequester be) {
+        return be.needsSorting();
     }
 }
