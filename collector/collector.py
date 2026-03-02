f32bb43e11e5e744a818970be1e3d35af08345df
YukkuriC <799433638@qq.com>
2026-03-02 21:47:59 +0800

    sysly?

--- collector/collector.py
+++ collector/collector.py
@@ -1,3 +1,8 @@
 from YukkuriC.minecraft.collector import *
 
-do_collect_simple(__file__)
+do_collect_simple(
+    __file__,
+    pattern='../build/libs/*-all.jar',
+    pattern_exist='*.jar',
+    renamer=lambda n: n.replace('-all.jar', '.jar'),
+)
